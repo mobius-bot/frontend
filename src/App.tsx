e7ccf02d631a992fe2a8630f10514de9ad4d9d69
UnclePhil1 <philipnssien@gmail.com>
2025-03-02 21:40:18 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+
+  return (
+    <>
+        <div className='bg-mute w-full h-screen flex justify-center items-center flex-col'>
+            <h1 className='text-4xl font-bold'>Mobius Bot</h1>
+            <p className='text-2xl'>Automate Your Trading on Sonic SVM</p>
+        </div>
+    </>
+  )
+}
+
+export default App
